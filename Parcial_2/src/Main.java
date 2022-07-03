e9e522a5bfc60041d2f5b8445bb0a07727985638
Federico Rossendy <fede07@users.noreply.github.com>
2022-07-03 19:46:57 -0300

    Parcial 2 Con fatiga

--- Parcial_2/src/Main.java
+++ Parcial_2/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
 	public static void main(String[] args) {
-		GestorArchivos archivo = new GestorArchivos("caso7");
+		GestorArchivos archivo = new GestorArchivos("DOOM");
 		Arbol arbol = archivo.leerArchivo();
 		arbol.analizarArbol();
 		archivo.escribirArchivo(arbol);
